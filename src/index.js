41d6e5ab975108ec272b4e8d5b3df2e054e22b9c
Joseph <josephsasson1@gmail.com>
2021-06-17 09:09:59 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newForm = document.querySelector('#create-task-form')
+  const newList = document.querySelector('#tasks')
+
+  newForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    const newTaskDescription = document.querySelector('#new-task-description').value
+    const li = document.createElement('li')
+    li.textContent = newTaskDescription
+    
+
+  const btn = document.createElement('button')
+  btn.textContent = 'X'
+  // btn.addEventListener('click', (e)=>{
+  //   e.target.parentElement.remove()
+  // })
+  btn.addEventListener('click', ()=>{
+    li.remove()
+  })
+  li.appendChild(btn)
+  newList.appendChild(li)
+  })
+  
 });
